90ecefbc89f5eec458bca5385a276616d3beedfb
madeval <alvarez.fing@gmail.com>
2023-04-30 20:16:34 -0300

    fix: :wrench: change the name of env variable

--- config/supabase_config.ts
+++ config/supabase_config.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY as string
+export const supabaseClient = createClient(supabaseUrl, supabaseKey)
