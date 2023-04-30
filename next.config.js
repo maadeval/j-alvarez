d3d8527167a6dd93e9b72aba090d64268dd1a860
madeval <alvarez.fing@gmail.com>
2023-04-30 20:09:59 -0300

    chore: :building_construction: add domain images variable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [process.env.NEXT_PUBLIC_SUPABASE_IMAGE_BUCKET],
+  },
 }
 
 module.exports = nextConfig
